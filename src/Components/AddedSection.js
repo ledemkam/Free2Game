d349ca83417e76e58f3f75855d9fe12ebb95cedd
OezcanEser <oe.eser84@gmail.com>
2021-06-28 09:12:49 +0200

    fixing

--- src/Components/AddedSection.js
+++ src/Components/AddedSection.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Cardcopy from './Cardcopy';
+import Card from './Card';
 
 class AddedSection extends Component {
     state = {
@@ -20,44 +20,8 @@ class AddedSection extends Component {
         return (
             <section id="addedGames" className="grid4" >
                 {this.state.isLoaded ?
-                    <Cardcopy
-                        image={this.state.data[0].thumbnail}
-                        title={this.state.data[0].title}
-                        description={this.state.data[0].short_description}
-                        // link={ }
-                        platform={this.state.data[0].platform}
-                        genre={this.state.data[0].genre}
-                    /> : "Loading ..."
-                }
-                {this.state.isLoaded ?
-                    <Cardcopy
-                        image={this.state.data[1].thumbnail}
-                        title={this.state.data[1].title}
-                        description={this.state.data[1].short_description}
-                        // link={ }
-                        platform={this.state.data[1].platform}
-                        genre={this.state.data[1].genre}
-                    /> : "Loading ..."
-                }
-                {this.state.isLoaded ?
-                    <Cardcopy
-                        image={this.state.data[2].thumbnail}
-                        title={this.state.data[2].title}
-                        description={this.state.data[2].short_description}
-                        // link={ }
-                        platform={this.state.data[2].platform}
-                        genre={this.state.data[2].genre}
-                    /> : "Loading ..."
-                }
-                {this.state.isLoaded ?
-                    <Cardcopy
-                        image={this.state.data[3].thumbnail}
-                        title={this.state.data[3].title}
-                        description={this.state.data[3].short_description}
-                        // link={ }
-                        platform={this.state.data[3].platform}
-                        genre={this.state.data[3].genre}
-                    /> : "Loading ..."
+                    this.state.data.slice(0, 4).map((ele, i) => <Card key={i} data={ele} />)
+                    : "Loading ..."
                 }
             </section>
         );
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,15 +6,15 @@ const Card = (props) => {
     return (
         <figure className="card">
             <img src={props.data.thumbnail} alt="" />
-            <section className="description">
+            <figcaption className="description">
                 <h3>{props.data.title}</h3>
                 <p>{props.data.short_description}</p>
-                <button></button>
-            </section>
-            <div className="category">
-                <span>{props.data.platform === "PC (Windows)" ? <img src="windows.png" alt="" /> : <img src="browser.png" alt="" />}</span>
-                <span>{props.data.genre}</span>
-            </div>
+                <button>Button</button>
+                <div className="category">
+                    <span>{props.data.platform === "PC (Windows)" ? <img src="windows.png" alt="" /> : <img src="browser.png" alt="" />}</span>
+                    <span>{props.data.genre}</span>
+                </div>
+            </figcaption>
         </figure>
     );
 }
