605d51575ed942c5ef2aee4af496387cd0ffe513
ledemkam <ledemkam@gmail.com>
2021-06-28 17:42:22 +0200

    merge

--- src/components/Allgames.js
+++ src/components/Allgames.js
@@ -1,7 +1,6 @@
 import Header from './Header';
 //import Button from './Button';
 import '../css/Allgames.css';
-<<<<<<< HEAD
 //import React, { Component } from 'react';
 import ApiAllgames from './Apiallgames';
 //import Card from './Card';
@@ -18,29 +17,6 @@ const Allgames = () => {
          <div>
             <div>
                <Select />
-=======
-import React, { Component } from 'react';
-import ApiAllgames from './Apiallgames'
-// import Card from './Card'
-import Select from './Selectmenu'
-import Select1 from './Selectmenu'
-import Select2 from './Selectmenu'
-
-
-const Allgames = () => {
-    return (
-        <div className="detailsgame">
-            <Header />
-            <div className="detailimg">
-                <img src="../img/allgames.png" alt="" />
-            </div>
-            <h1 className="allgamestitel">all games</h1>
-            <div>
-                <div>
-                    <Select /> <Select1 /><Select2 />
-                </div>
-                <ApiAllgames />
->>>>>>> 80a73f022c4d29b98a85d833d8a8fc42bf0737aa
             </div>
             <ApiAllgames />
          </div>
