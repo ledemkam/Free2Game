64958d9332793b61537dec6941b406bf58c8ecbe
OezcanEser <oe.eser84@gmail.com>
2021-06-29 23:17:12 +0200

    fixing

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,9 @@ const Header = () => {
     return (
         <>
             <div className="header">
-<<<<<<< HEAD
-                <img className="logo" src="./img/logo.png" alt="" />
-                <form action="">
-                    <input className="searchfield" type="text" />{' '}
-=======
                 <div><img className="logo" src="./img/logo.png" alt="" /></div>
                 <form action="">
                     <input className="searchfield" type="text" />
->>>>>>> main
                     <button><img className="searchicon" src="./img/search.png" alt="" /></button>
                 </form>
             </div>
