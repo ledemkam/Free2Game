bd5f972d02daa42ccf685bca1d63140195b9e558
OezcanEser <oe.eser84@gmail.com>
2021-06-30 10:28:53 +0200

    fixing

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -2,17 +2,17 @@ import '../../css/Navigation.css';
 import { VscHome } from 'react-icons/vsc';
 import { GiRetroController } from 'react-icons/gi';
 import { TiPlusOutline } from 'react-icons/ti';
+import { useState } from 'react'
 
 import {
    Link
 } from 'react-router-dom';
 
-// align - items: flex - end;
-
 const Navbar = () => {
+   const [isOpen, setIsOpen] = useState(false)
    return (
-      <nav>
-         <div className="burgerMenu">
+      <nav className={isOpen ? "open" : "closeNav"}>
+         <div className="burgerMenu" onClick={() => setIsOpen(!isOpen)}>
             <div />
             <div />
             <div />
@@ -21,19 +21,19 @@ const Navbar = () => {
             <li>
                <Link to="/">
                   <VscHome fontSize="30px" />
-                  Home
+                  <span style={{ display: isOpen ? "inline-block" : "none" }}>Home</span>
                </Link>
             </li>
             <li>
                <Link to="/Allgames">
                   <GiRetroController fontSize="30px" />
-                  All Game
+                  <span style={{ display: isOpen ? "inline-block" : "none" }}>All Game</span>
                </Link>
             </li>
             <li>
                <Link to="/AddedPage">
                   <TiPlusOutline fontSize="30px" />
-                  Recently Added
+                  <span style={{ display: isOpen ? "inline-block" : "none" }}>Recently Added</span>
                </Link>
             </li>
          </ul>
